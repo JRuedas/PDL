98262033b0c4b67b3dd8031e3ecaeb4a6cff9272
Guillermo Antonanzas <rigui@hotmail.es>
2017-01-24 16:08:09 +0100

    Algunos cambios

---
+++ src/ASemantico.py
@@ -0,0 +1,10 @@
+
+class Expr: pass
+
+class Program(Expr):
+    def __init__(self, parte1, parte2):
+        pass
+
+class Funcion(Expr):
+    def __init__(self, palr, tipo, name, atrib, sentenc):
+        pass
